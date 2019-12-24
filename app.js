993a864e55a209d1b8cdc57a1f6c7e49c659d2b5
Peter Slavov <slavovpeter2@gmail.com>
2019-12-23 19:06:19 -0500

    real attempted fix

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ let app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
 app.listen(process.env.PORT || 3000, function() {
   console.log(
     "Express server listening on port %d in %s mode",
