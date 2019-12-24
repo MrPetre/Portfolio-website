05a0751fd5795288be756b0a7f770381b91b824e
Peter Slavov <slavovpeter2@gmail.com>
2019-12-23 19:07:42 -0500

    further attempts

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 let express = require("express");
 let app = express();
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "public"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("public"));
 }
 
 app.get("*", (request, response) => {
-  response.sendFile(path.join(__dirname, "client/build", "index.html"));
+  response.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(process.env.PORT || 3000, function() {
