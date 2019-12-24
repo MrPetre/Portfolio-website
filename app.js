2eb2d84de2368f9271705536869233c19f3b66c1
Peter Slavov <slavovpeter2@gmail.com>
2019-12-23 19:03:33 -0500

    server side fix

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 let express = require("express");
 let app = express();
 
-app.use("/", express.static(__dirname + "/public"));
+//Look for statics first
+app.use(express.static(path.join(__dirname, "public")));
+//Return the index for any other GET request
+app.get("/*", function(req, res) {
+  res.sendFile("index.html", { root: path.join(__dirname, "public") });
+});
 
 app.listen(process.env.PORT || 3000, function() {
   console.log(
