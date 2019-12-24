f6ee84a5e57c8fcff95acbabaa8f7acb5f8715d2
Peter Slavov <slavovpeter2@gmail.com>
2019-12-23 19:01:06 -0500

    server side fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let express = require("express");
 let app = express();
 
-app.use(express.static(__dirname + "/public"));
+app.use("/", express.static(__dirname + "/public"));
 
 app.listen(process.env.PORT || 3000, function() {
   console.log(
