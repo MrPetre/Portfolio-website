b859a2afd5951f5f0335ad683795ba24f6094436
Peter Slavov <slavovpeter2@gmail.com>
2019-12-23 19:04:39 -0500

    server side revert

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 let express = require("express");
 let app = express();
 
-//Look for statics first
-app.use(express.static(path.join(__dirname, "public")));
-//Return the index for any other GET request
-app.get("/*", function(req, res) {
-  res.sendFile("index.html", { root: path.join(__dirname, "public") });
-});
+app.use(express.static(__dirname + "/public"));
 
 app.listen(process.env.PORT || 3000, function() {
   console.log(
