8e290545c16bfe5d3e3f9fe9cd2b36d7753a15e6
Peter Slavov <slavovpeter2@gmail.com>
2020-01-05 15:29:49 -0500

    DOM content fix

--- public/js/sr.js
+++ public/js/sr.js
@@ -1,79 +1,85 @@
-const defaultProps = {
-  easing: "cubic-bezier(0.5, 0, 0, 1)",
-  distance: "15px",
-  duration: 1000,
-  desktop: true,
-  mobile: true
-};
+document.addEventListener("DOMContentLoaded", function(event) {
+  function js_Load() {
+    document.body.style.visibility = "visible";
+  }
 
-ScrollReveal().reveal("#left-banner", {
-  ...defaultProps,
-  delay: 300,
-  origin: "left"
-});
+  const defaultProps = {
+    easing: "cubic-bezier(0.5, 0, 0, 1)",
+    distance: "15px",
+    duration: 1000,
+    desktop: true,
+    mobile: true
+  };
 
-ScrollReveal().reveal(".banner-image", {
-  ...defaultProps,
-  delay: 300,
-  origin: "right"
-});
+  ScrollReveal().reveal("#left-banner", {
+    ...defaultProps,
+    delay: 350,
+    origin: "left"
+  });
 
-ScrollReveal().reveal(".mobile-img-banner", {
-  ...defaultProps,
-  delay: 300,
-  origin: "right"
-});
+  ScrollReveal().reveal(".banner-image", {
+    ...defaultProps,
+    delay: 350,
+    origin: "right"
+  });
 
-ScrollReveal().reveal(".about-image", {
-  ...defaultProps,
-  delay: 600,
-  origin: "left"
-});
+  ScrollReveal().reveal(".mobile-img-banner", {
+    ...defaultProps,
+    delay: 350,
+    origin: "right"
+  });
 
-ScrollReveal().reveal(".about-me-title", {
-  ...defaultProps,
-  delay: 650,
-  origin: "top"
-});
+  ScrollReveal().reveal(".about-image", {
+    ...defaultProps,
+    delay: 600,
+    origin: "left"
+  });
 
-ScrollReveal().reveal(".services-title", {
-  ...defaultProps,
-  delay: 700,
-  origin: "top"
-});
+  ScrollReveal().reveal(".about-me-title", {
+    ...defaultProps,
+    delay: 650,
+    origin: "top"
+  });
 
-ScrollReveal().reveal(".services-list", {
-  ...defaultProps,
-  delay: 800,
-  origin: "left"
-});
+  ScrollReveal().reveal(".services-title", {
+    ...defaultProps,
+    delay: 700,
+    origin: "top"
+  });
 
-ScrollReveal().reveal(".blue-button", {
-  ...defaultProps,
-  delay: 400,
-  origin: "bottom"
-});
+  ScrollReveal().reveal(".services-list", {
+    ...defaultProps,
+    delay: 800,
+    origin: "left"
+  });
 
-ScrollReveal().reveal(".para", {
-  ...defaultProps,
-  delay: 700,
-  origin: "right"
-});
+  ScrollReveal().reveal(".blue-button", {
+    ...defaultProps,
+    delay: 400,
+    origin: "bottom"
+  });
 
-ScrollReveal().reveal(".project-wrapper__text", {
-  ...defaultProps,
-  delay: 600,
-  origin: "left"
-});
+  ScrollReveal().reveal(".para", {
+    ...defaultProps,
+    delay: 700,
+    origin: "right"
+  });
 
-ScrollReveal().reveal(".project-wrapper__image", {
-  ...defaultProps,
-  delay: 685,
-  origin: "right"
-});
+  ScrollReveal().reveal(".project-wrapper__text", {
+    ...defaultProps,
+    delay: 600,
+    origin: "left"
+  });
 
-ScrollReveal().reveal("footer", {
-  ...defaultProps,
-  delay: 400,
-  origin: "bottom"
+  ScrollReveal().reveal(".project-wrapper__image", {
+    ...defaultProps,
+    delay: 685,
+    origin: "right"
+  });
+
+  ScrollReveal().reveal("footer", {
+    ...defaultProps,
+    delay: 400,
+    origin: "bottom"
+  });
 });
